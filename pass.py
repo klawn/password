590fe990b4a326faf41b406b533ed982bedfe9db
Kimberly Lawn <klawn1570@gmail.com>
2019-03-07 11:19:24 -0600

    Initial commit, password functionality operational

---
+++ pass.py
@@ -0,0 +1,6 @@
+query = input("Please enter your password: ")
+password = 'Ginger'
+if query == password:
+    print("Password is correct.")
+else:
+    print("Incorrect password. Please check and try again.")
