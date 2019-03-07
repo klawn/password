313d21b4b5237430df1e6a118620b597a4cd9c1f
Kimberly Lawn <klawn1570@gmail.com>
2019-03-07 11:29:03 -0600

    added counter for number of incorrect guesses

--- pass.py
+++ pass.py
@@ -1,10 +1,13 @@
 query = input("Please enter your password: ")
 password = 'Ginger'
 correct = False
+counter = 0
 while correct == False:
     if query == password:
         print("Password is correct.")
         correct = True
     else:
         print("Incorrect password. Please check and try again.")
+        counter = counter+1
+        print("You have entered an incorrect password",counter,"times.")
         query = input("Please enter your password: ")
